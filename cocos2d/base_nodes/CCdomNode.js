1a327d673b20ce49e1fa5e7aa8874d734d8c8e40
sunzhuoshi <sunzhuoshi@gmail.com>
2014-11-06 21:05:49 +0800

    Fixed running error(s)

--- cocos2d/base_nodes/CCdomNode.js
+++ cocos2d/base_nodes/CCdomNode.js
@@ -225,7 +225,9 @@ cc.DOM.methods = /** @lends cc.DOM# */{
      */
     setParent:function (p) {
         this._parent = p;
-        p.setAnchorPoint(p.getAnchorPoint());
+        if (p) {
+            p.setAnchorPoint(p.getAnchorPoint());
+        }
         this.setNodeDirty();
         cc.DOM.parentDOM(this);
     },
